f533e8f2ea4d0022198b1a4e8bb65dba99b35a5f
Garuka404 <garukagimhana@gmail.com>
2025-04-10 18:18:44 +0530

    add ListContactSalaryTabel UI to display contacts sorted by salary
    - Created a JFrame-based UI to list contact details in a JTable.
    - Implemented DefaultTableModel for table data management.
    - Added sorting logic for contact salaries using bubble sort.
    - Integrated navigation to the Home Page via a 'Home Page' button.

---
+++ src/ListContactSalary.java
@@ -0,0 +1,97 @@
+
+
+import javax.swing.*;
+import javax.swing.table.*;
+import java.awt.*;
+import java.awt.event.*;
+
+class ListContactSalaryTabel extends JFrame{
+    private JButton homeButton;
+    private JTable tblCustomerDetails;
+    private DefaultTableModel dtm;
+    homePage HomePage;
+    ListContactNameTabel listContactNameTabel;
+    static ContactAdd  contactAdd=new ContactAdd();
+
+    public DefaultTableModel getDefaultTableModel(){
+        return dtm;
+    }
+    public void setDefaultTableModel(DefaultTableModel dtm){
+        this.dtm=dtm;
+    }
+
+
+    ListContactSalaryTabel(){
+        setSize(800, 500);
+        setTitle("Add Contact");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+
+        JPanel mainGrid=new JPanel(new GridLayout(2,1,10,10));
+
+        //////////////////////title
+        JPanel contentPanel=new JPanel();
+        contentPanel.setBackground(new Color(0,191,255));
+        contentPanel.setPreferredSize(new Dimension(270,65));
+        mainGrid.add("North",contentPanel);
+
+
+        JLabel UpdateContactTitle=new JLabel("LIST Contact By Salary");
+        UpdateContactTitle.setHorizontalAlignment(JLabel.CENTER);
+        UpdateContactTitle.setFont(new Font("",1,35));
+
+        contentPanel.add(UpdateContactTitle);
+        mainGrid.add(contentPanel);
+        add("North",mainGrid);
+
+        ////////////////////////////
+
+        String [] ColumnsName={"Customer Id","Name","Number","Company","Salary","date"};
+        dtm=new  DefaultTableModel(ColumnsName,0);
+
+        tblCustomerDetails=new JTable(dtm);
+        JScrollPane tabelPane=new JScrollPane(tblCustomerDetails);
+        add("Center",tabelPane);
+
+
+        JPanel btHomeName=new JPanel(new FlowLayout(FlowLayout.CENTER));
+        homeButton =new JButton("Home Page");
+        homeButton.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent evt){
+                if(HomePage==null){
+                    HomePage=new homePage();
+                    new  homePage().setVisible(false);
+                }
+                HomePage.setVisible(true);
+                dispose();
+            }});
+        homeButton.setPreferredSize(new Dimension(150,25));
+        homeButton.setFont(new Font("",1,12));
+
+        btHomeName.add(homeButton);
+        add("South",btHomeName);
+    }
+
+    public  void  sortSalary(){
+
+        for(int x=0;x<contactAdd.Contactinfro.length;x++){
+            for(int i=1;i<contactAdd.Contactinfro.length-x;i++){
+                if (contactAdd.Contactinfro[i-1].getCsalary().compareTo(contactAdd.Contactinfro[i].getCsalary()) > 0) {
+                    ContactAdd temp=contactAdd.Contactinfro[i-1];
+                    contactAdd.Contactinfro[i-1]=contactAdd.Contactinfro[i];
+                    contactAdd.Contactinfro[i]=temp;
+
+                }
+            }
+        }
+
+    }
+
+}
+
+public class ListContactSalary{
+    public static void main(String [] args){
+        new ListContactSalaryTabel().setVisible(true);
+    }
+}
